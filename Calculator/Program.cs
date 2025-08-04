e9064d76c5aaa87fc09456fb0903e7d6f6fe90d3
Dawid Płatek <dawidplatek.eu.pl@gmail.com>
2025-08-04 18:10:15 +0200

    Week 3 lesson 7: Basics

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,7 +10,32 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            byte number;
+            number = 255;
+            int number1 = 9999;
+            number1 = 123;
+            // Console.WriteLine(number1);
+            double number2 = 123.456;
+            decimal amount = 31.12m;
+            bool isReady = false;
+            string name = "Programista";
+            char letter = 'A';
+            /* DateTime today = new DateTime(2025, 8, 8, 12, 0, 0);
+            Robot robot = new Robot(); */
+            List<int> numbersList = new List<int>() { 1, 2, 3 };
+            numbersList.Add(4);
+            numbersList.Remove(2);
+            Console.WriteLine(numbersList[1]);
+            List<string> namesList = new List<string>() { "Jan", "Adam", "Ewa" };
+            int age = 20;
+            string myAge = "30";
+            // age = myAge; / błąd kompilacji, ponieważ typy są różne
+            age = int.Parse(myAge); // konwersja string na int
+            myAge = age.ToString(); // konwersja int na string
+
+
+
+
         }
     }
 }

9219f6a98f58198d96440c8683f5749440320bb1
Dawid Płatek <dawidplatek.eu.pl@gmail.com>
2025-09-17 22:35:47 +0200

    Week 3 lesson 8: Keyword var

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -12,23 +12,24 @@ namespace Calculator
         {
             byte number;
             number = 255;
-            int number1 = 9999;
+            var number1 = 9999;
+
             number1 = 123;
             // Console.WriteLine(number1);
-            double number2 = 123.456;
-            decimal amount = 31.12m;
-            bool isReady = false;
-            string name = "Programista";
-            char letter = 'A';
+            var number2 = 123.456;
+            var amount = 31.12m;
+            var isReady = false;
+            var name = "Programista";
+            var letter = 'A';
             /* DateTime today = new DateTime(2025, 8, 8, 12, 0, 0);
             Robot robot = new Robot(); */
-            List<int> numbersList = new List<int>() { 1, 2, 3 };
+            var numbersList = new List<int>() { 1, 2, 3 };
             numbersList.Add(4);
             numbersList.Remove(2);
             Console.WriteLine(numbersList[1]);
-            List<string> namesList = new List<string>() { "Jan", "Adam", "Ewa" };
-            int age = 20;
-            string myAge = "30";
+            var namesList = new List<string>() { "Jan", "Adam", "Ewa" };
+            var age = 20;
+            var myAge = "30";
             // age = myAge; / błąd kompilacji, ponieważ typy są różne
             age = int.Parse(myAge); // konwersja string na int
             myAge = age.ToString(); // konwersja int na string

eef9926e3210c4b30997675ff0909480c6ca698b
Dawid Płatek <dawidplatek.eu.pl@gmail.com>
2025-11-22 21:57:36 +0100

    Week 3 lesson 9: Introduction to Object-Oriented Programming

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,31 +10,37 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            byte number;
-            number = 255;
-            var number1 = 9999;
+            //byte number;
+            //number = 255;
+            //var number1 = 9999;
 
-            number1 = 123;
-            // Console.WriteLine(number1);
-            var number2 = 123.456;
-            var amount = 31.12m;
-            var isReady = false;
-            var name = "Programista";
-            var letter = 'A';
-            /* DateTime today = new DateTime(2025, 8, 8, 12, 0, 0);
-            Robot robot = new Robot(); */
-            var numbersList = new List<int>() { 1, 2, 3 };
-            numbersList.Add(4);
-            numbersList.Remove(2);
-            Console.WriteLine(numbersList[1]);
-            var namesList = new List<string>() { "Jan", "Adam", "Ewa" };
-            var age = 20;
-            var myAge = "30";
-            // age = myAge; / błąd kompilacji, ponieważ typy są różne
-            age = int.Parse(myAge); // konwersja string na int
-            myAge = age.ToString(); // konwersja int na string
+            //number1 = 123;
+            //// Console.WriteLine(number1);
+            //var number2 = 123.456;
+            //var amount = 31.12m;
+            //var isReady = false;
+            //var name = "Programista";
+            //var letter = 'A';
+            ///* DateTime today = new DateTime(2025, 8, 8, 12, 0, 0);
+            //Robot robot = new Robot(); */
+            //var numbersList = new List<int>() { 1, 2, 3 };
+            //numbersList.Add(4);
+            //numbersList.Remove(2);
+            //Console.WriteLine(numbersList[1]);
+            //var namesList = new List<string>() { "Jan", "Adam", "Ewa" };
+            //var age = 20;
+            //var myAge = "30";
+            //// age = myAge; / błąd kompilacji, ponieważ typy są różne
+            //age = int.Parse(myAge); // konwersja string na int
+            //myAge = age.ToString(); // konwersja int na string
 
 
+            var robot = new Robot("21234");
+            var robot2 = new Robot("232323234");
+            robot.SayHello();
+            robot2.SayHello();
+            robot.Name = "Robo1";
+            Console.WriteLine(robot.Name);
 
 
         }
--- Calculator/Robot.cs
+++ Calculator/Robot.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Calculator
 {
-    internal class Robot
+    class Robot
     {
+        private string _serialNumber;
+        public string Name;
+
+        public Robot(string serialNumber)
+        {
+            _serialNumber = serialNumber;
+            Console.WriteLine("Konstruktor.");
+        }
+        public void SayHello()
+        {
+            Console.WriteLine("Cześć, jestem robotem !!! Mój numer seryjny to " + _serialNumber);
+        }
+
+        public DateTime GetCurrentDate()
+        {
+            var currentTime = DateTime.Now;
+            return currentTime;
+        }
     }
 }
